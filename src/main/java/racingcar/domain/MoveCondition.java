e0d562aa442775091148c06f13f40c67d725a4f0
suzhanlee <108535068+suzhanlee@users.noreply.github.com>
2023-10-26 21:15:35 +0900

    feat : MoveCondition 전진 가능 여부 반환 기능 추가

---
+++ src/main/java/racingcar/domain/MoveCondition.java
@@ -0,0 +1,10 @@
+package racingcar.domain;
+
+public class MoveCondition {
+    public static final int START_MOVE_NUMBER = 4;
+    public static final int END_MOVE_NUMBER = 9;
+
+    public boolean checkNumber(int number) {
+        return number >= START_MOVE_NUMBER && number <= END_MOVE_NUMBER;
+    }
+}
---
+++ src/test/java/racingcar/domain/MoveConditionTest.java
@@ -0,0 +1,38 @@
+package racingcar.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class MoveConditionTest {
+
+    @ParameterizedTest
+    @DisplayName("0에서 9 사이의 값을 받아 값이 4 이상 경우 true 반환")
+    @ValueSource(ints = {4, 9})
+    void move(int given) {
+        // given
+        MoveCondition condition = new MoveCondition();
+
+        // when
+        boolean result = condition.checkNumber(given);
+
+        // then
+        assertThat(result).isEqualTo(true);
+    }
+
+    @ParameterizedTest
+    @DisplayName("0에서 9 사이의 값을 받아 값이 4 미만인 경우 false 반환")
+    @ValueSource(ints = {0, 3})
+    void cannotMove(int given) {
+        // given
+        MoveCondition condition = new MoveCondition();
+
+        // when
+        boolean result = condition.checkNumber(given);
+
+        // then
+        assertThat(result).isEqualTo(false);
+    }
+}
